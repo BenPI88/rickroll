3091cb1215c48f77c888cc4f6dc0feea7f6a3ec3
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-07-07 18:47:32 -0500

    Fixed Wait Time... ?

--- payload/open-vlc.py
+++ payload/open-vlc.py
@@ -1,3 +1,2 @@
 import os
-os.system("aplay yumbo.wav")
-os.system("export DISPLAY=:0" /n + "vlc -f --no-audio rickroll.wav")
+os.system("export DISPLAY=:0 && vlc -f --no-audio rickroll.wav && aplay yumbo.wav")
