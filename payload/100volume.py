01f727f40a4ddef310d1fdd86b1c28ab4262fc43
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-07-07 18:32:58 -0500

    Moved Some Files - Made Payloads Baked In

---
+++ payload/100volume.py
@@ -0,0 +1,4 @@
+import os
+while True:
+    os.system("amixer sset Master 100%")
+    os.system("amixer sset Master unmute")
---
+++ payload/open-vlc.py
@@ -0,0 +1,3 @@
+import os
+os.system("aplay yumbo.wav")
+os.system("export DISPLAY=:0" /n + "vlc -f --no-audio rickroll.wav")
