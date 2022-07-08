a700064b513d2a9f78a321c128d1e92bd80abade
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-07-08 09:35:08 -0500

    Opens rickroll in chromium

--- payload/exec-all.py
+++ payload/exec-all.py
@@ -1,2 +1,2 @@
 import os
-os.system("python 100volume.py & python open-vlc.py & delay 120 && chromium https://www.theraleighregister.com/get-rickrolled-m8.html")
+os.system("python 100volume.py & python open-vlc.py & chromium https://www.thisworldthesedays.com/when-is-he-coming-back-with-the-milk.html")
