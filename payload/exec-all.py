16cbacd4349da5b576bb76ff6af1f1c545ab794a
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-07-08 09:06:50 -0500

    Opens Chromium!

--- payload/exec-all.py
+++ payload/exec-all.py
@@ -1,2 +1,2 @@
 import os
-os.system("python 100volume.py & python open-vlc.py")
+os.system("python 100volume.py & python open-vlc.py & delay 120 && chromium https://www.theraleighregister.com/get-rickrolled-m8.html")
