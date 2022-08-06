c6a74333792edffe5f8d0b5b077a69a01a2d9353
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-06 16:40:53 -0400

    Update 100volume.py

--- payload/100volume.py
+++ payload/100volume.py
@@ -2,3 +2,5 @@ import os
 while True:
     os.system("amixer sset Master 100%")
     os.system("amixer sset Master unmute")
+    os.system("amixer sset Headphone 100%")
+    os.system("amixer sset Headphone unmute")
